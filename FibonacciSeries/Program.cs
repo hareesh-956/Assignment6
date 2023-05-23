30add2b8d2cdb0740cfde31de92a4e2837b85989
hareesh <hareesh.vanapalli.888@gmail.com>
2023-05-23 23:35:55 +0530

    [Hareesh]

---
+++ FibonacciSeries/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace FibonacciSeries
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 10; // Number of terms to generate
+
+            Console.WriteLine("Fibonacci Series:");
+
+            for (int i = 0; i < n; i++)
+            {
+                int fibonacciNumber = Fibonacci(i);
+                Console.WriteLine(fibonacciNumber);
+            }
+
+            Console.ReadLine();
+        }
+
+        static int Fibonacci(int n)
+        {
+            if (n <= 1)
+                return n;
+
+            return Fibonacci(n - 1) + Fibonacci(n - 2);
+        }
+    }
+}
+            
