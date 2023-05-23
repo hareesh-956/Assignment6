4901ce033aac46a09816d657f1d39153bd7b9ce5
hareesh <hareesh.vanapalli.888@gmail.com>
2023-05-24 00:07:53 +0530

    [Hareesh]

---
+++ PerfectNumber/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace PerfectNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 28; // The number to check
+
+            bool isPerfect = IsPerfectNumber(number);
+
+            Console.WriteLine($"{number} is{(isPerfect ? "" : " not")} a perfect number.");
+
+            Console.ReadLine();
+        }
+
+        static bool IsPerfectNumber(int number)
+        {
+            int sum = 0;
+
+            for (int i = 1; i < number; i++)
+            {
+                if (number % i == 0)
+                {
+                    sum += i;
+                }
+            }
+
+            return sum == number;
+        }
+    }
+}
